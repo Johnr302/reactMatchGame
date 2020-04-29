ee1ce27a491c0938712d8cc5533f6b8c8799f1aa
Ryan Regalado <ryanreg48@gmail.com>
2020-04-28 21:08:34 -0700

    feat(constants): added constants

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,52 +1,7 @@
 import React from "react";
 import "./card.css";
 import { ScoreConsumer } from "../scoreContext";
-
-const blueBGurl =
-  "https://wallpapertag.com/wallpaper/middle/5/8/b/836279-plain-blue-screen-wallpaper-1920x1080-1920x1080-full-hd.jpg";
-
-const cardArray = [
-  {
-    name: "suzuki",
-    img:
-      "https://i0.wp.com/www.asphaltandrubber.com/wp-content/uploads/2020/02/2020-Suzuki-GSX-RR-MotoGP-scaled.jpg?fit=2560%2C1707&ssl=",
-  },
-  {
-    name: "yamaha",
-    img:
-      "https://yamaha.scdn5.secure.raxcdn.com/library/img.jpg?id=5d1fbf682a0ab7466074b562&w=400",
-  },
-  {
-    name: "akira",
-    img:
-      "https://3.bp.blogspot.com/-whgpj2_RpXE/XMlg16UnC3I/AAAAAAAAaok/6NGeYH2TfkQSJWfQ6VKqydywBe18Y4CigCLcBGAs/s1600/Mercenary%2BGarage%2BCustom%2BMotorcycle%2BWorkshop%2B%2BAkira%2BKaneda%2BBike%2BKitbash%2Bby%2BJames%2BQiu%2Bon%2BBehance%2B01.jpg",
-  },
-  {
-    name: "mt10",
-    img:
-      "https://dd5394a0b8ca8e97ba29-abf76f3d91a2125517d6c7c409f095c7.ssl.cf1.rackcdn.com/content/common/Models/2020/f0926b63-edba-41cf-9a31-70d23677dcbf.png",
-  },
-  {
-    name: "suzuki",
-    img:
-      "https://i0.wp.com/www.asphaltandrubber.com/wp-content/uploads/2020/02/2020-Suzuki-GSX-RR-MotoGP-scaled.jpg?fit=2560%2C1707&ssl=",
-  },
-  {
-    name: "yamaha",
-    img:
-      "https://yamaha.scdn5.secure.raxcdn.com/library/img.jpg?id=5d1fbf682a0ab7466074b562&w=400",
-  },
-  {
-    name: "akira",
-    img:
-      "https://3.bp.blogspot.com/-whgpj2_RpXE/XMlg16UnC3I/AAAAAAAAaok/6NGeYH2TfkQSJWfQ6VKqydywBe18Y4CigCLcBGAs/s1600/Mercenary%2BGarage%2BCustom%2BMotorcycle%2BWorkshop%2B%2BAkira%2BKaneda%2BBike%2BKitbash%2Bby%2BJames%2BQiu%2Bon%2BBehance%2B01.jpg",
-  },
-  {
-    name: "mt10",
-    img:
-      "https://dd5394a0b8ca8e97ba29-abf76f3d91a2125517d6c7c409f095c7.ssl.cf1.rackcdn.com/content/common/Models/2020/f0926b63-edba-41cf-9a31-70d23677dcbf.png",
-  },
-];
+import constants from "./constants.js";
 
 let isMatch = [];
 
@@ -73,7 +28,7 @@ const flipCard = (event, value) => {
   }
   // const index = el.getAttribute("data-index");
   const cardId = el.getAttribute("data-id");
-  const display = cardArray.find((element) => element.name === cardId);
+  const display = constants.cardArray.find((element) => element.name === cardId);
   el.setAttribute("src", display.img);
   if (!isMatch.find((element) => element === el)) {
     // push to chose card array
@@ -93,7 +48,7 @@ const flipCard = (event, value) => {
         // if match increase score and make images white
         value.updateScore();
       } else {
-        isMatch.forEach((element) => element.setAttribute("src", blueBGurl));
+        isMatch.forEach((element) => element.setAttribute("src", constants.blueBGurl));
         ///else flip cards back over reset to blue
       }
       isMatch = [];
@@ -111,8 +66,8 @@ const Card = (props) => {
       {(value) => {
         return (
           <img
-            data-id={cardArray[index].name}
-            src={blueBGurl}
+            data-id={constants.cardArray[index].name}
+            src={constants.blueBGurl}
             className="card-container"
             alt=""
             data-index={index}
