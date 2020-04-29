2688c0d7f757f40dd913fd97a9c297e92ae6077b
Johnr302 <johnathon.regalado@gmail.com>
2020-04-28 22:04:22 -0700

    shuffle logic
    Signed-off-by: Johnr302 <johnathon.regalado@gmail.com>

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,13 +2,37 @@ import React from "react";
 import Card from "./Card.js";
 import "./board.css";
 
+// random int function
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// randomize array
+const randomizeArray = (num) => {
+  let results = [];
+  let tempArray = [];
+  // generate an array of number from 0 to num -1
+  for (let i = 0; i < num; i++) {
+    tempArray.push(i);
+  }
+  // need to use numbCards -1 and include 0 to create
+  // a random array of numbers
+  while (results.length < num) {
+    results.push(tempArray.splice(getRandomInt(tempArray.length), 1)[0]);
+  }
+
+  return results;
+};
+
 // makes number of cards passed in. Returns <Card /> component
 const makeCards = (num) => {
   // num = number of cards
+  let randomArray = randomizeArray(num);
   let results = [];
-
-  for (let i = 0; i < num; i++) {
-    results.push(<Card key={i.toString()} index={i} />);
+  for (let i = 0; i < randomArray.length; i++) {
+    results.push(
+      <Card key={randomArray[i].toString()} index={randomArray[i]} />
+    );
   }
   return results;
 };
--- src/components/constants.js
+++ src/components/constants.js
@@ -1,49 +1,48 @@
 const constants = {
   blueBGurl:
-  "https://wallpapertag.com/wallpaper/middle/5/8/b/836279-plain-blue-screen-wallpaper-1920x1080-1920x1080-full-hd.jpg",
+    "https://wallpapertag.com/wallpaper/middle/5/8/b/836279-plain-blue-screen-wallpaper-1920x1080-1920x1080-full-hd.jpg",
   cardArray: [
     {
       name: "suzuki",
       img:
-      "https://i0.wp.com/www.asphaltandrubber.com/wp-content/uploads/2020/02/2020-Suzuki-GSX-RR-MotoGP-scaled.jpg?fit=2560%2C1707&ssl=",
+        "https://i0.wp.com/www.asphaltandrubber.com/wp-content/uploads/2020/02/2020-Suzuki-GSX-RR-MotoGP-scaled.jpg?fit=2560%2C1707&ssl=",
     },
     {
       name: "yamaha",
       img:
-      "https://yamaha.scdn5.secure.raxcdn.com/library/img.jpg?id=5d1fbf682a0ab7466074b562&w=400",
+        "https://yamaha.scdn5.secure.raxcdn.com/library/img.jpg?id=5d1fbf682a0ab7466074b562&w=400",
     },
     {
       name: "akira",
       img:
-      "https://3.bp.blogspot.com/-whgpj2_RpXE/XMlg16UnC3I/AAAAAAAAaok/6NGeYH2TfkQSJWfQ6VKqydywBe18Y4CigCLcBGAs/s1600/Mercenary%2BGarage%2BCustom%2BMotorcycle%2BWorkshop%2B%2BAkira%2BKaneda%2BBike%2BKitbash%2Bby%2BJames%2BQiu%2Bon%2BBehance%2B01.jpg",
+        "https://3.bp.blogspot.com/-whgpj2_RpXE/XMlg16UnC3I/AAAAAAAAaok/6NGeYH2TfkQSJWfQ6VKqydywBe18Y4CigCLcBGAs/s1600/Mercenary%2BGarage%2BCustom%2BMotorcycle%2BWorkshop%2B%2BAkira%2BKaneda%2BBike%2BKitbash%2Bby%2BJames%2BQiu%2Bon%2BBehance%2B01.jpg",
     },
     {
       name: "mt10",
       img:
-      "https://dd5394a0b8ca8e97ba29-abf76f3d91a2125517d6c7c409f095c7.ssl.cf1.rackcdn.com/content/common/Models/2020/f0926b63-edba-41cf-9a31-70d23677dcbf.png",
+        "https://dd5394a0b8ca8e97ba29-abf76f3d91a2125517d6c7c409f095c7.ssl.cf1.rackcdn.com/content/common/Models/2020/f0926b63-edba-41cf-9a31-70d23677dcbf.png",
     },
     {
       name: "suzuki",
       img:
-      "https://i0.wp.com/www.asphaltandrubber.com/wp-content/uploads/2020/02/2020-Suzuki-GSX-RR-MotoGP-scaled.jpg?fit=2560%2C1707&ssl=",
+        "https://i0.wp.com/www.asphaltandrubber.com/wp-content/uploads/2020/02/2020-Suzuki-GSX-RR-MotoGP-scaled.jpg?fit=2560%2C1707&ssl=",
     },
     {
       name: "yamaha",
       img:
-      "https://yamaha.scdn5.secure.raxcdn.com/library/img.jpg?id=5d1fbf682a0ab7466074b562&w=400",
+        "https://yamaha.scdn5.secure.raxcdn.com/library/img.jpg?id=5d1fbf682a0ab7466074b562&w=400",
     },
     {
       name: "akira",
       img:
-      "https://3.bp.blogspot.com/-whgpj2_RpXE/XMlg16UnC3I/AAAAAAAAaok/6NGeYH2TfkQSJWfQ6VKqydywBe18Y4CigCLcBGAs/s1600/Mercenary%2BGarage%2BCustom%2BMotorcycle%2BWorkshop%2B%2BAkira%2BKaneda%2BBike%2BKitbash%2Bby%2BJames%2BQiu%2Bon%2BBehance%2B01.jpg",
+        "https://3.bp.blogspot.com/-whgpj2_RpXE/XMlg16UnC3I/AAAAAAAAaok/6NGeYH2TfkQSJWfQ6VKqydywBe18Y4CigCLcBGAs/s1600/Mercenary%2BGarage%2BCustom%2BMotorcycle%2BWorkshop%2B%2BAkira%2BKaneda%2BBike%2BKitbash%2Bby%2BJames%2BQiu%2Bon%2BBehance%2B01.jpg",
     },
     {
       name: "mt10",
       img:
-      "https://dd5394a0b8ca8e97ba29-abf76f3d91a2125517d6c7c409f095c7.ssl.cf1.rackcdn.com/content/common/Models/2020/f0926b63-edba-41cf-9a31-70d23677dcbf.png",
+        "https://dd5394a0b8ca8e97ba29-abf76f3d91a2125517d6c7c409f095c7.ssl.cf1.rackcdn.com/content/common/Models/2020/f0926b63-edba-41cf-9a31-70d23677dcbf.png",
     },
-  ]
-
+  ],
 };
 
-export default constants
+export default constants;
