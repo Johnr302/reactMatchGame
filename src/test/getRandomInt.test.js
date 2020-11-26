202bca3b947968efcadf16cfa4891ca50d37b393
Johnr302 <johnathon.regalado@gmail.com>
2020-11-25 18:31:23 -0800

    test complete for getRandomInt

---
+++ src/test/getRandomInt.test.js
@@ -0,0 +1,7 @@
+import getRandomInt from "../helperFunctions/getRandomInt";
+
+test("generates a random number between 0 and less than the number passed in", () => {
+  let result = getRandomInt(5);
+  expect(result).toBeLessThan(5);
+  expect(result).toBeGreaterThan(-1);
+});
