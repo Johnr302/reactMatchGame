2f1a1c0275a8a41356da1828756ede6dc8cac5a5
Johnr302 <johnathon.regalado@gmail.com>
2021-01-07 17:58:50 -0800

    complete testing for board

--- src/test/Board.test.js
+++ src/test/Board.test.js
@@ -1,9 +1,8 @@
 import React from "react";
 import Enzyme, { mount } from "enzyme";
-
-import { Board, makeCards } from "../components/Board";
+import { CARD_DATA } from "../card-constants.js";
+import { Board } from "../components/Board";
 import { Card } from "../components/Card";
-import { ScoreProvider } from "../scoreContext";
 
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
@@ -11,18 +10,12 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe("Board", () => {
   it("renders a <Board /> component", () => {
-    const wrapper = mount(
-      <ScoreProvider value={{ win: false }}>
-        <Board />
-      </ScoreProvider>
-    );
+    const wrapper = mount(<Board cards={CARD_DATA} />);
     expect(wrapper.find("#board")).toHaveLength(1);
   });
 
-  it("has a makeCards method that returns an array of 12 <Card /> components", () => {
-    let result = makeCards(12);
-    const wrapper = mount(result[0]);
-    expect(result).toHaveLength(12);
-    expect(wrapper.find(Card)).toHaveLength(1);
+  it("renders a <Board /> component that has 12 Card components", () => {
+    const wrapper = mount(<Board cards={CARD_DATA} />);
+    expect(wrapper.find(Card)).toHaveLength(12);
   });
 });
