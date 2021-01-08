f58927ac9fea3acbd5a4fb86067aafdda7dff823
Johnr302 <johnathon.regalado@gmail.com>
2021-01-07 19:19:37 -0800

    testing for NewGame component

---
+++ src/test/NewGame.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Enzyme, { mount } from "enzyme";
+import NewGame from "../components/NewGame";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import Button from "react-bootstrap/Button";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<NewGame />", () => {
+  it("renders <NewGame /> component", () => {
+    const wrapper = mount(<NewGame />);
+    expect(wrapper.find(NewGame)).toHaveLength(1);
+  });
+
+  it("renders <NewGame /> component that has a button", () => {
+    const wrapper = mount(<NewGame />);
+    console.log(wrapper.debug());
+    expect(wrapper.find("Button")).toHaveLength(1);
+  });
+
+  it("has a button that take a click handler", () => {
+    const onClick = jest.fn();
+    const wrapper = mount(<Button onClick={onClick} />);
+    wrapper.find(Button).simulate("click");
+    expect(onClick).toHaveBeenCalled();
+  });
+});
